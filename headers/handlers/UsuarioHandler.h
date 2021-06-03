cee0c2f856c3a82d651b9fd98718a922e04a6a33
fabriciodepalma <fabriciodepalma@outlook.com>
2021-06-02 23:01:53 -0300

    correccion carpeta

---
+++ headers/handlers/UsuarioHandler.h
@@ -0,0 +1,28 @@
+#ifndef DATA_USUARIOHANDLER_H
+#define DATA_USUARIOHANDLER_H
+
+#include <string>
+#include <map>
+#include <set>
+#include <utility>
+
+#include "../enums.h"
+#include "../datatypes/DataVideojuego.h"
+//#include "../datatypes/DataFecha.h"
+
+using namespace std;
+
+class UsuarioHandler
+{
+private:
+    //set<Usuario> users;
+
+public: /*FALTA
+    
+    DataUsuario(string email, string password);
+    string getEmail();
+    string getPassword();
+    ~DataUsuario();*/
+};
+
+#endif
