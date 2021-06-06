3d983e0d6d42d22ff08273586d872383c5d28ceb
Christian <christian.tejeda@fing.edu.uy>
2021-06-06 19:13:55 -0300

    Setters jugador

--- src/entities/Categoria.cpp
+++ src/entities/Categoria.cpp
@@ -10,21 +10,28 @@
 #include "../../headers/datatypes/DataCategoria.h"
 #include "../../headers/utils/Fecha.h"
 
-Categoria::Categoria()
-{
-}
-Categoria::Categoria(std::string nombre, std::string descripcion)
-{
+Categoria::Categoria() { }
+
+Categoria::Categoria(std::string nombre, std::string descripcion) {
     this->nombre = nombre;
     this->descripcion = descripcion;
 }
-std::string Categoria::getNombre()
-{
+
+std::string Categoria::getNombre() {
     return this->nombre;
 }
+
 std::string Categoria::getDescripcion()
 {
     return this->descripcion;
+}
+
+void Categoria::setNombre(std::string nombre) {
+    this->nombre = nombre;
+}
+
+void Categoria::setDescipcion(std::string descripcion) {
+    this->descripcion = descripcion;
 }
 
 DataCategoria Categoria::getData()
