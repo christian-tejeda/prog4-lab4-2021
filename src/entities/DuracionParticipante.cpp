8a39bfa4d07de9d894115350d93fb7458ed8fcf0
martin.pineyro <martin.pineyro@fing.edu.uy>
2021-06-09 01:09:21 -0300

    duracion participante conflicto resuelto

--- src/entities/DuracionParticipante.cpp
+++ src/entities/DuracionParticipante.cpp
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#include "../../headers/entities/DuracionParticipante.h"
-#include <string>
-
-DuracionParticipante::DuracionParticipante(){}
-//Getters
-DuracionParticipante::DuracionParticipante(Fecha &he,Fecha &hs,Jugador* jg){}
-Fecha DuracionParticipante::getHoraEntrada(){}
-Fecha DuracionParticipante::getHoraSalida(){}
-Jugador* DuracionParticipante::getParticipante(){}
-//Setters
-void DuracionParticipante::setHoraEntrada(Fecha &f){}
-void DuracionParticipante::setHoraSalida(Fecha &f){}
-void DuracionParticipante::setParticipante(Jugador* prt){}
-void DuracionParticipante::terminarParticipacion(Fecha &f){}
-
-DuracionParticipante::~DuracionParticipante(){}
-=======
 
 #include <string>
 #include <map>
@@ -29,38 +11,45 @@ DuracionParticipante::~DuracionParticipante(){}
 #include "../../headers/datatypes/DataDesarrollador.h"
 #include "../../headers/utils/Fecha.h"
 
-DuracionParticipante::DuracionParticipante(){}
-DuracionParticipante::DuracionParticipante(Fecha &he, Fecha &hs, Jugador *jg){
-        this->horaEntrada=he;
-        this->horaSalida=hs;
-        this->participante=jg;
-    }
+DuracionParticipante::DuracionParticipante() {}
+DuracionParticipante::DuracionParticipante(Fecha &he, Fecha &hs, Jugador *jg)
+{
+    this->horaEntrada = he;
+    this->horaSalida = hs;
+    this->participante = jg;
+}
 
-    //Getters
-    Fecha DuracionParticipante::getHoraEntrada(){
-        return this->horaEntrada;
-    }
-    Fecha DuracionParticipante::getHoraSalida(){
-        return this->horaSalida;
-    }
-    Jugador * DuracionParticipante::getParticipante(){
-        return this->participante;
-    }
+//Getters
+Fecha DuracionParticipante::getHoraEntrada()
+{
+    return this->horaEntrada;
+}
+Fecha DuracionParticipante::getHoraSalida()
+{
+    return this->horaSalida;
+}
+Jugador *DuracionParticipante::getParticipante()
+{
+    return this->participante;
+}
 
-    //Setters
-    void DuracionParticipante::setHoraEntrada(Fecha &f){
-        this->horaEntrada=f;
-    }
-    void DuracionParticipante::setHoraSalida(Fecha &f){
-        this->horaSalida=f;
-    }
-    void DuracionParticipante::setParticipante(Jugador *prt){
-        this->participante=prt;
-    }
+//Setters
+void DuracionParticipante::setHoraEntrada(Fecha &f)
+{
+    this->horaEntrada = f;
+}
+void DuracionParticipante::setHoraSalida(Fecha &f)
+{
+    this->horaSalida = f;
+}
+void DuracionParticipante::setParticipante(Jugador *prt)
+{
+    this->participante = prt;
+}
 
-    void DuracionParticipante::terminarParticipacion(Fecha &f){
-        this->horaSalida=f;
-    }
+void DuracionParticipante::terminarParticipacion(Fecha &f)
+{
+    this->horaSalida = f;
+}
 
-    DuracionParticipante::~DuracionParticipante(){}
->>>>>>> 40f0bf4582fe2e3b8754ed27b995905bae6f1e0e
+DuracionParticipante::~DuracionParticipante() {}
